a443d3bcbca9af009a39a313e0df2a199d2c0540
Fernando Romo <112670507+fromo0214@users.noreply.github.com>
2023-11-02 21:45:23 -0700

    Delete EmployeeSkills_T.sql

--- EmployeeSkills_T.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE EmployeeSkills_T(
-	Employee_ID int NOT NULL,
-    SkillID int NOT NULL,
-CONSTRAINT EmployeeSkills_PK PRIMARY KEY (Employee_ID, SkillID),
-CONSTRAINT EmployeeSkills_FK1 FOREIGN KEY (Employee_ID) REFERENCES
-Employee(EmployeeID),
-CONSTRAINT EmployeeSkills_FK2 FOREIGN KEY (SkillID) REFERENCES
-Skill(SkillID));
